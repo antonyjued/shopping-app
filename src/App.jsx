ade7ca2267a367e31bf55ce420d38a8d117d4392
antonyjued <antonyjayakumar4@gmail.com>
2024-09-04 20:40:04 +0530

    testing

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Card from './card.jsx'
+
+function App() {
+
+
+  return (
+    <div className="App-Container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+import Logo from './assets/react.svg'
+
+import './Card.css'
+function Card(){
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+  
+  return (
+    <div className="card">
+      <img src={Logo} className="react-logo"/><br/>
+        <button onClick={() => setCount1((count1) => count1 + 1)}>
+          Add Cart : {count1}
+        </button>
+        <button onClick={() => setCount2((count2) => count2 + 1)}>
+          Add Orders : {count2}
+        </button>
+    </div>
+    )
+  
+}
+
+export default Card
